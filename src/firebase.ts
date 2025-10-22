61cf38d0f3c0da2d29e58b9d0ca88cbbbc92f6ec
Ruben Olano <rubendavid2711@gmail.com>
2025-10-22 13:01:52 -0500

    Add authentication using token, must match userid

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,12 +1,12 @@
-import { initializeApp, cert } from 'firebase-admin/app'
-import { getFirestore } from 'firebase-admin/firestore'
+import { initializeApp, cert } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import { getAuth } from "firebase-admin/auth";
 
-const serviceAccount = require('../firebase-creds.json')
+const serviceAccount = require("../firebase-creds.json");
 
 initializeApp({
-  credential: cert(serviceAccount),
-})
+	credential: cert(serviceAccount),
+});
 
-const db = getFirestore()
-
-export { db }
+export const db = getFirestore();
+export const auth = getAuth();
---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { RequestHandler } from "express";
+import { auth } from "./firebase";
+
+export const validateRequest: RequestHandler = async (req, res, next) => {
+	const authHeader = req.headers.authorization;
+	const userId = req.get("X-User-Id");
+	if (!authHeader || !userId) {
+		return res.status(401).json({ error: "Unauthorized" });
+	}
+	const token = authHeader.split(" ")[1];
+	if (!token) {
+		return res.status(401).json({ error: "Unauthorized" });
+	}
+	const decodedToken = await auth.verifyIdToken(token);
+	// This should throw if no token is provided but just in case :)
+	if (!decodedToken || decodedToken.uid !== userId) {
+		return res.status(401).json({ error: "Unauthorized" });
+	}
+	next();
+};
