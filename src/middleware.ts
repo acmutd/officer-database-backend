ea476f1b31673e7e90f38bf9dabd6ce2d00ed942
Jeydin21 <JeyPham21@gmail.com>
2025-11-13 13:16:30 -0600

    refactor: Rewrite middleware as a wrapper for the functions

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,32 @@
-import { RequestHandler, Request, Response } from "express";
+import { Request, Response } from "@google-cloud/functions-framework";
 import { auth } from "./firebase";
 
-export const validateRequest: RequestHandler = async (req: Request, res: Response, next) => {
-	const authHeader = req.headers.authorization;
-	const userId = req.get("X-User-Id");
-	if (!authHeader || !userId) {
-		return res.status(401).json({ error: "Unauthorized" });
-	}
-	const token = authHeader.split(" ")[1];
-	if (!token) {
-		return res.status(401).json({ error: "Unauthorized" });
-	}
-	const decodedToken = await auth.verifyIdToken(token);
-	// This should throw if no token is provided but just in case :)
-	if (!decodedToken || decodedToken.uid !== userId) {
-		return res.status(401).json({ error: "Unauthorized" });
-	}
-	next();
+type Handler = (req: Request, res: Response) => Promise<void> | void;
+
+export const withAuth = (handler: Handler) => {
+	return async (req: Request, res: Response) => {
+		const authHeader = req.headers.authorization;
+		const userId = req.get("X-User-Id");
+
+		if (!authHeader || !userId) {
+			return res.status(401).json({ error: "Unauthorized" });
+		}
+
+		const token = authHeader.split(" ")[1];
+		if (!token) {
+			return res.status(401).json({ error: "Unauthorized" });
+		}
+
+		try {
+			const decodedToken = await auth.verifyIdToken(token);
+			if (!decodedToken || decodedToken.uid !== userId) {
+				return res.status(401).json({ error: "Unauthorized" });
+			}
+
+			// Validation passed, call the actual function
+			return handler(req, res);
+		} catch (error) {
+			return res.status(401).json({ error: "Unauthorized" });
+		}
+	};
 };
