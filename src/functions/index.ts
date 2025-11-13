fc0401733a653c08bba9a081c31261fd009a60dc
Jeydin21 <JeyPham21@gmail.com>
2025-11-13 15:52:04 -0600

    refactor: This thing called barrel export

---
+++ src/functions/index.ts
@@ -0,0 +1,5 @@
+export { createOfficer } from './createOfficer';
+export { getOfficer } from './getOfficer';
+export { getOfficers } from './getOfficers';
+export { updateOfficer } from './updateOfficer';
+export { deleteOfficer } from './deleteOfficer';
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-export { createOfficer } from './functions/createOfficer';
-export { getOfficer } from './functions/getOfficer';
-export { getOfficers } from './functions/getOfficers';
-export { updateOfficer } from './functions/updateOfficer';
-export { deleteOfficer } from './functions/deleteOfficer';
+export {
+  createOfficer,
+  getOfficer,
+  getOfficers,
+  updateOfficer,
+  deleteOfficer
+} from './functions';
