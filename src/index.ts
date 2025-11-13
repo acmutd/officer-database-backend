6e5b1abc71f6f536dd590fb064d6fff47c134624
Jeydin21 <JeyPham21@gmail.com>
2025-11-13 15:46:10 -0600

    feat: Add function exports for GCF

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { createOfficer } from './functions/createOfficer';
+export { getOfficer } from './functions/getOfficer';
+export { getOfficers } from './functions/getOfficers';
+export { updateOfficer } from './functions/updateOfficer';
+export { deleteOfficer } from './functions/deleteOfficer';
