aae23e15301e81a7095b62934fc4df5b8d3095bb
Jeydin21 <jeydin@j21.dev>
2025-11-13 21:03:37 -0600

    refactor: Recreate function initialization

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,7 @@
 import admin from "firebase-admin";
-import * as path from "path";
 
 if (!admin.apps.length) {
-  if (process.env.NODE_ENV === 'development' || !process.env.GOOGLE_APPLICATION_CREDENTIALS) {
-    admin.initializeApp({
-      credential: admin.credential.cert(
-        path.resolve(__dirname, '../firebase-creds.json')
-      )
-    });
-  } else {
-    admin.initializeApp({
-      credential: admin.credential.applicationDefault(),
-    });
-  }
+  admin.initializeApp();
 }
 
 export const auth = admin.auth();
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-export {
-  createOfficer,
-  getOfficer,
-  getOfficers,
-  updateOfficer,
-  deleteOfficer
-} from './functions';
+import { http } from '@google-cloud/functions-framework';
+
+import { createOfficer } from './functions/createOfficer';
+import { getOfficer } from './functions/getOfficer';
+import { getOfficers } from './functions/getOfficers';
+import { updateOfficer } from './functions/updateOfficer';
+import { deleteOfficer } from './functions/deleteOfficer';
+
+http('createOfficer', createOfficer);
+http('getOfficer', getOfficer);
+http('getOfficers', getOfficers);
+http('updateOfficer', updateOfficer);
+http('deleteOfficer', deleteOfficer);
+
+export { createOfficer, getOfficer, getOfficers, updateOfficer, deleteOfficer };
